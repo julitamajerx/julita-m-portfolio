be2e3edeee8d060ef513a3fe65b5a4320656dbcb
ScApeGoAT666 <julitamajer@gmail.com>
2026-03-04 19:43:03 +0100

    angular mat proj

--- src/features/projects/angularmaterial/angularmaterial.ts
+++ src/features/projects/angularmaterial/angularmaterial.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { ProjectCard } from '../../project-card/project-card';
+import { Carousel } from '../../carousel/carousel';
 
 @Component({
   selector: 'app-angularmaterial',
-  imports: [],
+  imports: [ProjectCard, Carousel],
   templateUrl: './angularmaterial.html',
   styleUrl: './angularmaterial.scss',
 })
-export class Angularmaterial {}
+export class Angularmaterial {
+  protected nbpSlides = [
+    { image: 'nbp2.png', description: 'Kalkulator walut.' },
+    { image: 'nbp3.png', description: 'Wersja mobilna.' },
+  ];
+}
